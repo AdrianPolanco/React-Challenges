5ba0bb90803a2718030923c852ff1146b0b4d2fa
Adrián Saul Polanco Ferrer <Adrian>
2023-11-01 15:58:05 -0400

    Progress Bar Challenge-Start

---
+++ progress-bar-ts/src/components/InputPercentage.tsx
@@ -0,0 +1,5 @@
+const InputPercentage = (): JSX.Element =>{
+    return <input type="number" className="border border-solid border-red-500 rounded-md w-2/12 xl:w-1/12 lg:w-2/12 md:w-2/12 sm:w-2/12"/>
+}
+
+export default InputPercentage
---
+++ progress-bar-ts/src/components/ProgressBar.tsx
@@ -0,0 +1,7 @@
+const ProgressBar = ():JSX.Element =>{
+    return <div className="border border-solid border-red-500 rounded-md w-10/12 sm:w-6/12 md:w-6/12 lg:w-6/12 xl:w-6/12 h-5">
+        
+    </div>
+}
+
+export default ProgressBar
